09a1be0988b870d902d4036652885c7c8e89f1be
Lourenço Vaz Pato <lourencopatto@gmail.com>
2018-10-20 19:03:05 +0100

    Reorganizei o codigo, faltava converter o tempo de microsegundos para segundos

--- Lab4/feedback_control_loop/feedback_control_loop.ino
+++ Lab4/feedback_control_loop/feedback_control_loop.ino
@@ -1,44 +1,153 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+// Constants
 const int LED1 = 5; // LED connected to analog pin5
 const int LDRpin = A5; // LDR connected to A5 pin
-float refValue = 0;
+float refValue = 0; // Desired illuminance in Lux
 const int Vcc = 5;
 
-// ldr1 parameters
+/// LDR1 parameters
 const float C = 25.8439;
 const float m = -0.4934;
+
+/// System parameters: Gain and Time constant function of illuminance x [ARDUINO 1]
+// G0(x) = mG1*x + bG1
+// Tau(x) = mtau1*x + btau1
 const float mG1 = 0.1001;
-const float bG1 = 44.977;
+const float bG1 = 44.9773;
+const double mtau1 = -0.00001449;
+const double btau1 = 0.0119;
+// Returns system gain for desired illuminance x
+float G0(float x) {
+  return mG1*x + bG1;
+}
+// Returns the time constant for desired illuminance x
+double tau(float x) {
+  return mtau1*x + btau1;
+}
 
-const float mtau1 = -pow(10, -5);
-const float btau1 = 0.0119;
 
+/// Tensão e tempo inicial (antes do escalão)
 float xi = 0;
 int ti = 0;
 
-float K = 0;
-
 int increment = 0;
 
+/// PI Controller
+// PI controller memory
 float y_ant = 0;
 float i_ant = 0;
 float e_ant = 0;
-
-// Adjust this
+// PI Controller parameters
 const float Kp = 1;
 const float Ki = 1;
 
+// Sampling Period
 const float T = 0.005;
+
+// Setup Interrupt
+volatile bool flag;
+ISR(TIMER1_COMPA_vect) {
+  flag = 1; //notify main loop
+}
+
+// Equivalent of map function for floats
+float mapfloat(double val, double in_min, double in_max, double out_min, double out_max) {
+  return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// Reads illuminance value from sensor, returns value in Lux
+float readLDR() {
+  // Read LDR voltage
+  int LDR = analogRead(A5);
+  float Vi = mapfloat(LDR, 0, 1023, 0, 5);
+
+  // Convert voltage into Lux
+  float base = (Vcc / Vi - 1);
+  float exponent = 1 / m;
+  float LuxValue = C * pow(base, exponent);
+  return LuxValue;
+}
+
+
+// Computes the controller value for the feedback system
+// Sums the Feed Forward controller with the PI Controller
+float Controller(float ref, float measuredY) {
+
+  // Calculate Feed Forward controller input tension
+  float Uff = FeedForwardController(ref);
+  Serial.print("Uff: ");
+  Serial.println(Uff);
+
+  // Compute error
+  float Upi = PIcontroller(calculateDesiredY(ref), lux2volt(measuredY));
+  Serial.print("Upi: ");
+  Serial.println(Upi);
+
+  // Compute PI controller input tension
+  float u = (Upi + Uff);
+  return u;
+}
+
+// Converts value in Lux to Volt
+float lux2volt(float lux) {
+  float base = lux/C;
+  float exponent = m;
+  return Vcc/(pow(base, exponent) + 1);
+}
+
+/// Computes the predicted output according to step response of system model G(s)
+//  G(s) = G0(x) / 1+Tau(x)*s
+//  v*(t) = vf - (vf-vi) * e^-(t-ti)/Tau(xf)
+float calculateDesiredY(float xf) {
+  float Vf = lux2volt(xf);
+  float Vi = lux2volt(xi);
+  double taux = tau(xf);
+  float t = micros();
+  
+  // The time constant Tau is in seconds therefore time must be converted from microseconds to seconds!!! 
+  return Vf - (Vf - Vi) * exp(-(t-ti)*0.000001/taux);     
+}
+
+
+// Feed Forward Controller, returns the output voltage to obtain refValue illuminance
+float FeedForwardController(float refValue) {
+  // Calculate Ui value
+  float Kff = 1 / (mG1 * refValue + bG1);
+  return Kff * refValue;
+}
+
+// PI Controller, returns the output voltage to obtain ref=y
+float PIcontroller(float ref, float y) {
+  float K1 = Kp * G0(ref);
+  Serial.print("K1: ");
+  Serial.println(K1);
+  float K2 = Kp * Ki * (T / 2);
+
+  float e = ref - y;
+  Serial.print("Error: ");
+  Serial.println(e);
+  float p = (K1 * ref) - (Kp * y);
+  Serial.print("p: ");
+  Serial.println(p);
+  float i = i_ant + K2 * (e + e_ant);
+  Serial.print("i: ");
+  Serial.println(i);
+  float u = p + i;
+  y_ant = y;
+  i_ant = i;
+  e_ant = e;
+
+  return u;
+}
+
 
 void setup() {
   // Setup pinmodes
   pinMode(LED1, OUTPUT);
   pinMode(LDRpin, INPUT);
   Serial.begin(250000);
-
-  float tglobal = micros();
 
   // Setup Timer interrupt (200 Hz)
   cli(); // stop interrupts
@@ -64,101 +173,6 @@ void setup() {
   }
 }
 
-volatile bool flag;
-
-// Interrupt
-ISR(TIMER1_COMPA_vect) {
-  flag = 1; //notify main loop
-}
-
-float mapfloat(double val, double in_min, double in_max, double out_min, double out_max) {
-  return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-
-// Reads illuminance value from sensor in Lux
-float readLDR() {
-  // Read LDR voltage
-  int LDR = analogRead(A5);
-  float Vi = mapfloat(LDR, 0, 1023, 0, 5);
-
-  // Convert voltage into Lux
-  float base = (Vcc / Vi - 1);
-  float exponent = 1 / m;
-
-  float LuxValue = C * pow(base, exponent);
-
-  return LuxValue;
-}
-
-
-
-float Controller(float ref, float measuredY) {
-
-  // Calculate Feed Forward controller input tension
-  float Uff = FeedForwardController(ref);
-  Serial.print("Uff: ");
-  Serial.println(Uff);
-
-  // Compute error
-  float Upi = PIcontroller(calculateDesiredY(ref), lux2volt(measuredY));
-  Serial.print("Upi: ");
-  Serial.println(Upi);
-
-  // Compute PI controller input tension
-  float u = (Upi + Uff);
-  return u;
-}
-
-float lux2volt(float lux) {
-  return Vcc / ( pow((lux / C), m) + 1);
-}
-
-float calculateDesiredY(float xf) {
-  float Vf = lux2volt(xf);
-  float Vi = lux2volt(xi);
-
-  float tau = mtau1 * xf + btau1;
-  float t = micros();
-
-  return Vf - (Vf - Vi) * exp(-(t - ti) / tau);
-}
-
-float G0(float x) {
-  return (mG1 * x) + bG1;
-}
-
-float FeedForwardController(float refValue) {
-  // Calculate Ui value
-  K = 1 / (mG1 * refValue + bG1);
-  float Ui = K * refValue;
-
-  return Ui;
-}
-
-float PIcontroller(float ref, float y) {
-  float K1 = Kp * G0(ref);
-  Serial.print("K1: ");
-  Serial.println(K1);
-  float K2 = Kp * Ki * (T / 2);
-
-  float e = ref - y;
-  Serial.print("Error: ");
-  Serial.println(e);
-  float p = (K1 * ref) - (Kp * y);
-  Serial.print("p: ");
-  Serial.println(p);
-  float i = i_ant + K2 * (e + e_ant);
-  Serial.print("i: ");
-  Serial.println(i);
-  float u = p + i;
-  y_ant = y;
-  i_ant = i;
-  e_ant = e;
-
-  return u;
-}
-
 void loop() {
   if (flag)
   {
@@ -177,6 +191,7 @@ void loop() {
     // Write output to system
     analogWrite(LED1, u);
 
+    // Register initial time of step input
     if (increment == 0) {
       ti = micros();
     }
