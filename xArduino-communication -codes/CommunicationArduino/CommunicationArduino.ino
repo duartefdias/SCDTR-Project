5b68a070239f442f7f3248d6e70ab25be792294a
Lourenço Vaz Pato <lourencopatto@gmail.com>
2018-11-30 17:20:45 +0000

    Arduino comms bug fix

--- xArduino-communication -codes/CommunicationArduino/CommunicationArduino.ino
+++ xArduino-communication -codes/CommunicationArduino/CommunicationArduino.ino
@@ -1,130 +1,43 @@
-<<<<<<< HEAD
 #include <Wire.h>
 #include <EEPROM.h>
  
-uint8_t own_addr = EEPROM.read(0);;
-uint8_t rcAdress, rcMessageType, rcPwmValue, rcPwmNegotiation;
-uint16_t rcLuxValue = 0;
-
- 
-void setup() {
-    Serial.begin(9600);
-    Wire.begin(own_addr);
-    TWAR = (own_addr << 1) | 1;  // enable broadcasts to be received
-    Wire.onReceive(receiveEvent);   // set up receive handler
-}
- 
-void loop() {
-  sendPeriodicalLuxValue(100);
-  sendPeriodicalPwmValue(255);
-  sendPeriodicalNegotioation(200);
-  delay(1500);
-}
-
-void receiveEvent(int howMany) {
-  // we are expecting 3 or 4 bytes, so check we got them
-  if (howMany == 3 || howMany == 4) {
-     rcAdress = Wire.read();
-     rcMessageType = Wire.read();
-     if(rcMessageType == 0) {
-        rcPwmValue = Wire.read();
-        rcPwmValue = map(rcPwmValue, 0, 255, 0, 5);
-        Serial.print("Received PWM value from ");
-        Serial.println(rcAdress);
-        Serial.print("PWM value: ");
-        Serial.println(rcPwmValue);
-        Serial.println();
-     }
-     else if(rcMessageType == 1) {
-        rcLuxValue = Wire.read();
-        rcLuxValue <<= 8;
-        rcLuxValue |= Wire.read();
-        Serial.print("Received Lux value from ");
-        Serial.println(rcAdress);
-        Serial.print("LUX value: ");
-        Serial.println(rcLuxValue);
-        Serial.println();       
-     }
-     else if(rcMessageType == 2) {
-        rcPwmNegotiation= Wire.read();
-        Serial.print("Received PWM negotiation from ");
-        Serial.println(rcAdress);
-        Serial.print("PWM negotiation: ");
-        Serial.println(rcPwmNegotiation);
-        Serial.println();       
-     }
-
-  //use values to do something
-  
-  }
-// throw away any garbage
-while (Wire.available() > 0) 
-  Wire.read();    
-}
-
-
-void sendPeriodicalPwmValue(uint8_t pwmValue){
-  Wire.beginTransmission(0);//get BUS
-  Wire.write(own_addr);
-  uint8_t messageType = 0;
-  Wire.write(messageType);
-  Wire.write(pwmValue);
-  Wire.endTransmission(); //release BUS
-}
-
-void sendPeriodicalLuxValue(uint16_t luxValue){
-  Wire.beginTransmission(0);//get BUS
-  Wire.write(own_addr);
-  uint8_t messageType = 1;
-  Wire.write(messageType);
-  uint8_t luxValueH = luxValue >> 8;
-  uint8_t luxValueL = luxValue & 0xFF;
-  Wire.write(luxValueH);
-  Wire.write(luxValueL);
-  Wire.endTransmission(); //release BUS
-}
-
-void sendPeriodicalNegotioation(uint8_t pwmNegotiation){
-  Wire.beginTransmission(0);//get BUS
-  Wire.write(own_addr);
-  uint8_t messageType = 2;
-  Wire.write(messageType);
-  Wire.write(pwmNegotiation);
-  Wire.endTransmission(); //release BUS
-}
-=======
-#include <Wire.h>
-#include <EEPROM.h>
- 
-uint8_t own_addr = EEPROM.read(0);;
-uint8_t rcAdress, rcMessageType, rcPwmValue, rcPwmNegotiation, rcOccupancy;
+uint8_t own_addr = EEPROM.read(0);
+uint8_t rcAddress, rcMessageType, rcPwmValue, rcPwmNegotiation, rcOccupancy;
 uint16_t rcLuxValue = 0, rcLuxLowerBound = 0, rcLuxBackground = 0, rcLuxRef = 0;
+float rcPwm = 0, rcPwmN = 0;
+
+float mapfloat(double val, double in_min, double in_max, double out_min, double out_max) {
+  return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 void setup() {
-    Wire.begin(own_addr);
-    TWAR = (own_addr << 1) | 1;  // enable broadcasts to be received
-    Wire.onReceive(receiveEvent);   // set up receive handler
+  Serial.begin(9600);
+  Wire.begin(own_addr);
+  TWAR = (own_addr << 1) | 1;  // enable broadcasts to be received
+  Wire.onReceive(receiveEvent);   // set up receive handler
 }
  
 void loop() {
-  sendLuxValue(340);
-  sendPwmValue(255);
-  sendPwmNegotioation(200);
+  sendLuxValue(100);
+  sendPwm(255);
+  sendNegotiation(200);
   sendOccupancy(0);
   sendLuxLowerBound(100);
-  sendLuxBackground(75);
-  sendLuxRef(340);
+  sendLuxBackground(5);
+  sendLuxRef(100);
   delay(1500);
 }
 
 void receiveEvent(int howMany) {  
   // we are expecting 3 or 4 bytes, so check we got them
   if (howMany == 3 || howMany == 4) {
-     rcAdress = Wire.read();
+     rcAddress = Wire.read();
      rcMessageType = Wire.read();
+     
      //pwm
      if(rcMessageType == 0) {
         rcPwmValue = Wire.read();
+        rcPwm = mapfloat(rcPwmValue, 0, 255, 0, 5);
      }
      //lux
      else if(rcMessageType == 1) {
@@ -135,6 +48,7 @@ void receiveEvent(int howMany) {
      //pwm negotiation
      else if(rcMessageType == 2) {
         rcPwmNegotiation= Wire.read();
+        rcPwm = mapfloat(rcPwmNegotiation, 0, 255, 0, 5);
      }
      //occupancy
      else if(rcMessageType == 3) {
@@ -163,13 +77,13 @@ void receiveEvent(int howMany) {
   //use values to do something
   
   }
-// throw away any garbage
-while (Wire.available() > 0) 
-  Wire.read();    
+  // throw away any garbage
+  while (Wire.available() > 0) 
+    Wire.read();    
 }
 
 
-void sendPwmValue(uint8_t pwmValue){
+void sendPwm(uint8_t pwmValue){
   Wire.beginTransmission(0);//get BUS
   Wire.write(own_addr);
   Wire.write(0);  //message type
@@ -188,7 +102,7 @@ void sendLuxValue(uint16_t luxValue){
   Wire.endTransmission(); //release BUS
 }
 
-void sendPwmNegotioation(uint8_t pwmNegotiation){
+void sendNegotiation(uint8_t pwmNegotiation){
   Wire.beginTransmission(0);//get BUS
   Wire.write(own_addr);
   Wire.write(2);  //message type
@@ -226,7 +140,7 @@ void sendLuxBackground(uint16_t luxBackground){
   Wire.endTransmission(); //release BUS
 }
 
-  void sendLuxRef(uint16_t luxRef){
+void sendLuxRef(uint16_t luxRef){
   Wire.beginTransmission(0);//get BUS
   Wire.write(own_addr);
   Wire.write(6);  //message type
@@ -235,5 +149,4 @@ void sendLuxBackground(uint16_t luxBackground){
   Wire.write(luxH);
   Wire.write(luxL);
   Wire.endTransmission(); //release BUS
-  }
->>>>>>> master
+}
