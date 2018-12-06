71cfdf7a1f67a0c9f1812236f1c5a0b384726c9c
DuartDx <duartexdias@gmail.com>
2018-12-06 16:54:51 +0000

    working on server

---
+++ RPI-codes/4-InitialServer/headers/networkFunctions.h
@@ -0,0 +1,19 @@
+#ifndef NETWORKFUNCTIONS_H
+#define NETWORKFUNCTIONS_H
+
+class Server {
+public:
+
+    Server(string IpAddress, string port);
+
+    void startAccept();
+    void handleAccept(session* sess, const error_code& ec);
+    void startListener();
+
+
+private:
+
+    
+};
+
+#endif //NETWORKFUNCTIONS_H
---
+++ RPI-codes/4-InitialServer/src/networkFunctions.cpp
@@ -0,0 +1,20 @@
+
+
+class Server {
+    io_service& io;
+    tcp::acceptor acceptor;
+    void start_accept() {
+        session* new_sess = new session(io);
+        acceptor.async_accept(new_sess->socket(),
+        boost::bind(&server::handle_accept, this, new_sess,_1));
+    }
+    void handle_accept(session* sess, const error_code& ec) {
+        if (!ec) sess->start(); else delete sess;
+            start_accept();
+    }
+    public:
+        server(io_service& io, short port)
+        : io(io), acceptor(io, tcp::endpoint(tcp::v4(), port)) {
+            start_accept();
+    }
+};
