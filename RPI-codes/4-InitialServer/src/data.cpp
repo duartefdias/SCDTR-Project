7db0daee00bd6c0a97047c3af9619fe6a4b568cd
Duarte Dias <DuartexDias@gmail.com>
2018-12-12 11:42:46 +0000

    initial classes

---
+++ RPI-codes/4-InitialServer/src/data.cpp
@@ -0,0 +1,9 @@
+#include "../headers/data.h"
+
+#include <iostream> //SYNC_TCP_SERVER.CPP
+#include <string>
+
+class Data{
+
+
+};
---
+++ RPI-codes/4-InitialServer/src/i2cFunctions.cpp
@@ -0,0 +1,9 @@
+#include "../headers/i2cFunctions.h"
+
+#include <iostream> //SYNC_TCP_SERVER.CPP
+#include <string>
+
+class I2cFunctions{
+
+
+};
