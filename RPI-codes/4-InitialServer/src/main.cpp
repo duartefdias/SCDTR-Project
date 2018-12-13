720a47ec379b12f220f9028b886e340edc3ccf75
Duarte Dias <DuartexDias@gmail.com>
2018-12-13 16:44:48 +0000

    debugging

--- RPI-codes/4-InitialServer/src/main.cpp
+++ RPI-codes/4-InitialServer/src/main.cpp
@@ -65,10 +65,10 @@ int main() {
             std::cout << "Received message: " << buf << std::endl;
             if(buf[2] == 'l'){
                 if(buf[4] == '1'){
-                    write(s, buffer(std::to_string(database.getLastLuxValueArduino1()),n), ec);
+                    write(s, buffer('f',n), ec);
                 }
                 else if(buf[4] == '2'){
-                    write(s, buffer(std::to_string(database.getLastLuxValueArduino2()),n), ec);
+                    write(s, buffer('merda',n), ec);
                 }
             }
             write(s, buffer(buf,n), ec);
