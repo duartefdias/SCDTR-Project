0400327d8d18f5588bb42fca8d36a5c8cde68d65
Lourenço Vaz Pato <lourencopatto@gmail.com>
2018-12-17 23:29:06 +0000

    desperate attempts at fixing the getting stuck bug

--- Lab5-I2C/MT-SR/multi_master_I2C/multi_master_I2C.ino
+++ Lab5-I2C/MT-SR/multi_master_I2C/multi_master_I2C.ino
@@ -5,7 +5,7 @@
 #include <I2C.h>
 #include <EEPROM.h>
 
-const int other_add = 0; //broadcast address
+uint8_t other_add = 0; //broadcast address
 int own_address; //this dev address
 char buff[8]; 
 String c = "";
@@ -13,11 +13,16 @@ char buf[8];
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   own_address = EEPROM.read(0); // Arduino's address is enconded in address 0 of EEPROM memory
   Serial.print("Connected, my adress is: ");
   Serial.println(own_address);
-    
+
+  //Send
+  I2c.begin();
+  I2c.timeOut(200);
+  //I2c.scan();
+  //receive
   Wire.begin(own_address); // join as a master
   TWAR = (own_address << 1)|1; // enable broadcasts to be received
   Wire.onReceive(receiveEvent); //event handler
@@ -28,18 +33,15 @@ void loop() {
   if(Serial.available() > 0 ) {
     c = Serial.readString();
     c.toCharArray(buf,8);
-    
-    Wire.beginTransmission(other_add);//get BUS
-    Wire.write(buf); //send byte to address on BUS
-    Serial.println(buf);
-    Wire.endTransmission(); //release BUS
+
+    I2c.write(other_add, 0x00, buf);
   }
 }
 
 void receiveEvent(int howMany) {
+  Serial.print("received: ");
   while(Wire.available() > 0) { //check data on BUS
     char c = Wire.read(); //receive byte at I2C BUS
     Serial.print(c); // echo on terminal
   }
-  Serial.println();
 }
