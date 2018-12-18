12102b1a64c5398ba94f23d2e9135fd28d03ef8e
Duarte Dias <DuartexDias@gmail.com>
2018-12-18 13:55:43 +0000

    back to spaces...

--- RPI-codes/4-InitialServer/src/data.cpp
+++ RPI-codes/4-InitialServer/src/data.cpp
@@ -197,6 +197,6 @@ std::string Data::processRequest(char* request){
             response = "Invalid request";
     }
     
-    response = response + "\0";
+    response = response + "                                  ";
     return response;
 }
