c57d39e3a2fe99abc96e806e7f1c4c06f96cfc7b
Duarte Dias <DuartexDias@gmail.com>
2018-12-20 03:38:35 +0000

    trying to get db into server classes for client response

--- RPI-codes/5-MultipleClients/headers/server.h
+++ RPI-codes/5-MultipleClients/headers/server.h
@@ -12,48 +12,53 @@ class session {
     ip::tcp::socket s;
     enum { max_len = 1024 };
     char data[max_len];
-    Data* db;
+    Database* db;
 
+    // Sends response to client
     void hread(const error_code& ec, size_t sz) {
-        if (!ec) async_write(s, buffer(db->processRequest(data), sz),
-        boost::bind(&session::hwrite, this, _1));
-        else delete this; 
+        if (!ec){
+            async_write(s, buffer(db->processRequest(data), sz), boost::bind(&session::hwrite, this, _1));
+        }
+        else delete this;
     }
 
+    // Happens after the write
     void hwrite(const error_code& ec) {
-        if (!ec) start();
+        if (!ec) {
+            start();
+        }
         else delete this; 
     }
 
     public:
-    session(io_service& io, Data* database) : s(io) {db = database;}
+    session(io_service& io, Data* database) : s(io) {db = database}
     ip::tcp::socket& socket() {return s;}
 
     void start() {
-        s.async_read_some(buffer(data,max_len),
-        boost::bind(&session::hread, this, _1, _2));
+        s.async_read_some(buffer(data,max_len), boost::bind(&session::hread, this, _1, _2));
     }
 };
 
 class server {
     io_service& io;
     ip::tcp::acceptor acc;
+    Data* database;
 
-    void start_accept(Data* database = nullptr) {
+    void start_accept(Data* database) {
         session* new_sess = new session(io, database);
-        acc.async_accept(new_sess->socket(),
-        boost::bind(&server::haccept, this, new_sess,_1)); 
+        acc.async_accept(new_sess->socket(), boost::bind(&server::haccept, this, new_sess,_1)); 
     }
 
     void haccept(session* sess, const error_code& ec) {
         if (!ec) sess->start();
         else delete sess;
-        start_accept(); 
+        start_accept(database); 
     }
 
     public:
     server(io_service& io, short port, Data* database)
     : io(io), acc(io, ip::tcp::endpoint(ip::address::from_string("127.0.0.1"), port)) {
-        start_accept(database); 
+        db = database;
+        start_accept(db); 
     }
 };
