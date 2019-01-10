5b716d34b40eb851fa36a178506acd8767d53b11
Lourenço Vaz Pato <lourencopatto@gmail.com>
2019-01-10 14:39:16 +0000

    Local Controller report

--- Lab4-Controlo/Jitter/jitter.m
+++ Lab4-Controlo/Jitter/jitter.m
@@ -1,11 +1,23 @@
+nbins = 21;
+
 load jitterUno.csv
+subplot(1,2,1);
+j1 = std(jitterUno);
+t1 = mean(jitterUno);
+% fprintf('Arduino Uno\n');
+% fprintf('Average sampling period of %s ms\n', num2str(t1/1000));
+% fprintf('Jitter of %s us\n', num2str(j1));
+histogram(jitterUno,nbins,'Normalization','probability');
+xlabel('Sampling Period [\mus]');
+title('Luminaire 1');
 
-std(jitterUno)
-mean(jitterUno)
-histogram(jitterUno,100)
-
+subplot(1,2,2);
 load jitterDue.csv
-
-std(jitterDue)
-mean(jitterDue)
-histogram(jitterDue,100)
+j2 = std(jitterDue);
+t2 = mean(jitterDue);
+% fprintf('\nArduino Due\n');
+% fprintf('Average sampling period of %s ms\n', num2str(t2/1000));
+% fprintf('Jitter of %s us\n', num2str(j2));
+histogram(jitterDue,nbins,'Normalization','probability');
+xlabel('Sampling Period [\mus]');
+title('Luminaire 2');
